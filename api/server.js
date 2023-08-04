a9b16c9c554b55d3c8f56eb7be1f5827eb841364
nav.yayy <navyaaasrivastav@gmail.com>
2023-08-05 00:53:31 +0530

    done

--- api/server.js
+++ api/server.js
@@ -7,13 +7,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(cors());
-// // Add this before server.use(router)
-// server.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//     "/blog/:resource/:id/show": "/:resource/:id",
-//   })
-// );
 server.use(router);
 server.listen(3006, () => {
   console.log("JSON Server is running");
